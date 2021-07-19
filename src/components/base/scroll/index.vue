19db3837714874f0ad7dc1b01989947e9faa0db5
zhangzeyi <zzy@paramland.com>
2021-07-20 00:03:32 +0800

    轮播图 列表  懒加载    准备加入自定义loading指令

---
+++ src/components/base/scroll/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div ref="rootRef">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+import useScroll from './use-scroll.js'
+import { ref } from 'vue'
+
+export default {
+  name: 'scroll',
+  props: {
+    click: {
+      type: Boolean,
+      default: true
+    }
+  },
+  setup(props) {
+    const rootRef = ref(null)
+    useScroll(rootRef, props)
+    return {
+      rootRef // 上面定义好rootRef 这里一定要return 不然不起作用
+    }
+  }
+}
+</script>
